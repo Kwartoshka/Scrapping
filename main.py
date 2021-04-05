e2865d5b824153e222a4530cff98283bb93b23e1
Andrey Zaytsev <and.zaytsev9@gmail.com>
2021-04-05 23:09:47 +0300

    prewiew Done

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+from datetime import date, timedelta
+
+if __name__ == '__main__':
+
+    KEYWORDS = ['дизайн', 'фото', 'web', 'python', 'php', 'id']
+    KEYWORDS = set(KEYWORDS)
+
+    url = 'https://habr.com/ru/all/'
+    result = requests.get(url)
+    page = BeautifulSoup(result.text, 'html.parser')
+    articles = page.find_all('article')
+    for article in articles:
+        res = (article.text.strip().lower())
+        check = set(re.findall(r'[а-яёА-ЯЁa-zA-Z]+', res))
+        if bool(check & KEYWORDS):
+            url = article.find(class_="post__title_link")
+            href =  url.attrs.get('href')
+            title = url.text
+            day = article.find(class_="post__time").text
+            if 'сегодня' in day:
+                day = date.today()
+            elif 'вчера' in day:
+                minus = timedelta(days=1)
+                day = date.today() - minus
+            print(f'{day} - {title} - {href}')
+
+
+
